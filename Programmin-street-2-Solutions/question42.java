de6d1c72fc9b5c830c270f8dc6b7d2b5839a3f99
Prince Pandey <princepandey6150@gmail.com>
2024-09-09 12:37:07 +0530

    qustion42.java added

---
+++ Programmin-street-2-Solutions/question42.java
@@ -0,0 +1,26 @@
+public class question42{
+    public static void main(String args[])
+    {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr[0].length; j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < arr.length; i++)
+        {
+            int greatest = Integer.MIN_VALUE;
+            for(int j = 0; j < arr[0].length; j++)
+            {
+                if(arr[i][j] > greatest)
+                {
+                    greatest = arr[i][j];
+                }
+            }
+            System.out.println(greatest);
+        }
+    }
+}
---
+++ Programmin-street-2-Solutions/question47.java
@@ -0,0 +1,18 @@
+
+sdfaefasdfa
+public class question47 {
+    public static void main(String args[])
+    {
+        int arr[] = {-1, 0, 1, 2, -1, -4};
+       for(int i  = arr.length-1; i >0; i++)
+       {
+           int sum = arr[i] + arr[i-1] + arr[i-2];
+           if( sum == 0)
+           {
+            System.out.println("[ " + arr[i-2] +", "+ arr[i-1] + ", " + arr[i] + " ]");
+           }
+       }
+    }sfasdfagaefasf
+    ..s.dfa;fasfasf;sfasdfagaefasfasfsa
+    
+}

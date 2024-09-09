cb6bbcc184339c57c3bd510fef1377f88048b44b
Prince Pandey <princepandey6150@gmail.com>
2024-09-09 18:07:36 +0530

    question13.java added

---
+++ Programming-Street-3-Solutions/question13.java
@@ -0,0 +1,17 @@
+public class question13{
+    public static void main(String args[])
+    {
+        int rows = 3; 
+        int temp = 1;
+        for(int i = 1; i <= rows; i++)
+        {
+            for(int j = 1; j <= rows; j++)
+            {
+                System.out.print(temp+" ");
+                temp++;
+            }
+            System.out.println();
+        }
+       
+    }
+}

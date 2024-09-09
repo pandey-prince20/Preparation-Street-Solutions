e04cbe33fa063fc3d0f9a0b2719f0bf7462c105d
Prince Pandey <princepandey6150@gmail.com>
2024-09-09 17:58:31 +0530

    quesiton6.java added

--- Programming-Street-3-Solutions/question2.java
+++ Programming-Street-3-Solutions/question2.java
@@ -1,10 +1,10 @@
-public class question1{
+public class question2{
     public static void main(String args[])
     {
-        int rows = 4;
+        int rows = 5;
         for(int i = 1; i <= rows; i++)
         {
-            for(int j = 1; j <= i; j++)
+            for(int j = 1; j <= rows; j++)
             {
                 System.out.print("* ");
             }

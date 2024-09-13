5eb0414898b0da1c507e72888cee860c3c6ce99b
Prince Pandey <princepandey6150@gmail.com>
2024-09-13 22:03:28 +0530

    question3.java added

--- Programming-Street-3-Solutions/question3.java
+++ Programming-Street-3-Solutions/question3.java
@@ -4,15 +4,15 @@ public class question3{
         int rows = 3;
         for(int i = 1; i <= rows; i++)
         {
-            for(int j = 1; j <= rows/2 + 1 + i; j++)
-            {
-                for(int k = 1; k <= rows/2 + 1 -j; k++)
-                {
-                    System.out.print(" ");
-                }
-                System.out.print(" * ");
-            }
-            System.out.println();
+           for(int j = rows -i; j > 0; j--)
+           {
+            System.out.print(" ");
+           }
+           for(int j = 2* i -1; j > 0; j--)
+           {
+            System.out.print("*");
+           }
+           System.out.println();
         }
     }
 }

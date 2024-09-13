5f7831bc94bebcac36663ffd336bef8dff724fda
Prince Pandey <princepandey6150@gmail.com>
2024-09-13 22:35:01 +0530

    question48.java added

---
+++ Programming-Street-3-Solutions/question48.java
@@ -0,0 +1,26 @@
+public class question48 {
+    public static void main(String args[])
+    {
+        int rows = 5; 
+        for(int i = 1; i <= rows; i++)
+        {
+            if( i != 1  && i != rows)
+            {
+                System.out.print("*");
+                for(int j = 1; j <= rows -2; j++)
+                {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            else
+            {
+                for(int j = 1; j <= rows; j++)
+                {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

0819e9dd2a0da1d514ed473a45527c279519946f
Prince Pandey <princepandey6150@gmail.com>
2024-09-13 13:19:52 +0530

    question42.java added

---
+++ question42.java
@@ -0,0 +1,31 @@
+public class question42{
+    public static void main(String args[])
+    {
+        int rows = 4;
+        boolean b = true;
+        for(int i = 1; i <= rows; i++)
+        {
+            for(int j = 1; j <= rows; j++)
+            {
+                if( b == true)
+                {
+                    System.out.print("A ");
+                    b = false;
+                }
+                else if( b== false)
+                {
+                    System.out.print("B ");
+                    b = true;
+                }
+            }
+            if( b == true)
+            {
+                b = false;
+            }
+            else{
+                b = true;
+            }
+            System.out.println();
+        }
+    }
+}
